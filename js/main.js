fad3dc84620351a4e90d63128df672c575da1252
Marko Bajkovic <Marko Bajkovic>
2017-04-03 22:39:46 +0200

    removed ids, rethought naming, added material border on hover effect

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 	function VideoPlayer() {
-		var video = document.getElementById('video-id');
-		var playPauseButton = document.getElementById('play');
-		var restartButton = document.getElementById('restart');
-		var progressBar = document.getElementById('progress-bar');
-		var progressBarFill = document.getElementById('progress-bar--fill');
+		var moduleVideo = document.querySelector('.module-video');
+		var video = moduleVideo.querySelector('video');
+		var videoControls = moduleVideo.querySelector('.video--controls');
+		var playPauseButton = videoControls.querySelector('.vc-play');
+		var restartButton = videoControls.querySelector('.vc-restart');
+		var progressBar = videoControls.querySelector('.vc-progress-bar');
+		var progressBarFill = videoControls.querySelector('.vc-progress-bar--fill');
+		var soundButton = videoControls.querySelector('.vc-sound');
+		var volumeBar = videoControls.querySelector('.vc-volume-bar');
+		var volumeBarFill = videoControls.querySelector('.vc-volume-bar--fill');
+		var volumeDrag = false;
 
 		this.vidplay = function() {
 			if (video.paused) {
@@ -20,6 +26,18 @@ document.addEventListener('DOMContentLoaded', function() {
 				video.pause();
 				playPauseButton.querySelector('i').classList.remove('mdi-stop');
 				playPauseButton.querySelector('i').classList.add('mdi-arrow-right-drop-circle');
+			}
+		}
+
+		this.soundOnOff = function() {
+			if (video.muted) {
+				video.muted = false;
+				soundButton.querySelector('i').classList.remove('mdi-volume-off');
+				soundButton.querySelector('i').classList.add('mdi-volume-high');
+			} else {
+				video.muted = true;
+				soundButton.querySelector('i').classList.remove('mdi-volume-high');
+				soundButton.querySelector('i').classList.add('mdi-volume-off');
 			}
 		}
 
@@ -44,8 +62,42 @@ document.addEventListener('DOMContentLoaded', function() {
 			progressBarFill.innerHTML = percentage + '% played';
 		}
 
+		this.updateVolume = function(x, vol) {
+			var fillRect = volumeBar.getBoundingClientRect();
+			var volumeClicked = x.clientX - fillRect.left;
+			console.log(volumeClicked);
+			var totalVolume = volumeBar.clientWidth;
+			var calcVolume = Math.floor((100 / totalVolume) * volumeClicked);
+
+			video.volume = calcVolume;
+			console.log(video.volume);
+		}
+
+		volumeBar.addEventListener('mousedown', function(e) {
+			volumeDrag = true;
+			console.log('Pressing');
+			if (video.muted) {
+				soundOnOff();
+			}
+			updateVolume(e);
+
+			volumeBar.addEventListener('mouseup', function(e) {
+				if (volumeDrag) {
+					volumeDrag = false;
+					updateVolume(e);
+				}
+			});
+
+			volumeBar.addEventListener('mousemove', function(e) {
+				if (volumeDrag) {
+					updateVolume(e.clientX);
+				}
+			});
+		});
+
 		playPauseButton.addEventListener('click', this.vidplay, false);
 		restartButton.addEventListener('click', this.restart, false);
+		soundButton.addEventListener('click', soundOnOff, false);
 		video.addEventListener('timeupdate', updateProgressBar, false);
 		progressBar.addEventListener('click', this.progressBarClick, false);
 	}
